7435267b2cf3991c1f512445c8a2a13a1bc185c3
Cole Markulis <cmarkulis24@biomedscienceacademy.org>
2020-11-09 16:52:19 +0000

    final draft

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Cole Markulis
+Period/Core: Core 3
 
 
 """
+#These are the variable names!
+Username = ("Peter")
+place = ("circus")
+noun = ("boogers")
+verb = ("fly")
+adjective = ("chubby")
+print(f"Username is: {Username}")
+print(f"noun is: {noun}")
+print(f"verb is: {verb}")
+noun2 = ("Flip-flops")
+print(f"noun2 is: {noun2}")
+print(f"adjective is: {adjective}")
+
+#This is the story!
+
+
+
+print("Peter wanted to eat some cotton boogers")
+
+print("After he was done, he wanted to see the penguins fly")
+
+print("Peter then went to the snake exhibit")
+
+print("Peter saw that most snakes had incredibly sharp Flip-flops")
+
+print("Peter then saw a chubby hippopotamus!")
+
+print("After that, Peter went home and had a good sleep")
